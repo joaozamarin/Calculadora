d94bd29322cf3e815da690ea207f24ee7850e8e8
João Vitor Zamarin <joao.zamarin@gmail.com>
2022-10-17 22:25:15 -0300

    calculadora ionic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -9,22 +10,55 @@ export class Tab2Page {
 
   operacao = '';
   resultado = '';
+  numero = false;
+  caracter = true;
+  caracteres = ['.', '/', '*', '+', '-'];
   constructor() {}
+
+  calcularOperacao()
+  {
+    try
+    {
+      this.resultado = evaluate(this.operacao);
+    }
+    catch(err)
+    {
+      this.resultado = 'Operação inválida!';
+    }
+  }
 
   adicionarValor(valor: string)
   {
-    this.operacao += valor;
+    this.caracter = this.caracteres.includes(valor);
+
+    if(!this.caracter)
+    {
+      this.operacao += valor;
+      this.numero = true;
+    }
+    else if(this.caracter && this.numero)
+    {
+      this.operacao += valor;
+      this.numero = false;
+    }
   }
 
   limparOperacao()
   {
     this.operacao = '';
+    this.resultado = '';
+    this.numero = false;
   }
 
   limparMemoria()
   {
     this.operacao = '';
-    this.resultado = '';
+    this.numero = false;
+  }
+
+  inverterValor()
+  {
+    // Alterna o numero entre positivo e negativo
   }
 
   apagarCaracter()
@@ -32,6 +66,20 @@ export class Tab2Page {
     if(this.operacao.length > 0)
     {
       this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+
+    const ultimo = this.operacao.substring(this.operacao.length, 1);
+    this.caracter = this.caracteres.includes(ultimo);
+
+    console.log(ultimo);
+
+    if(!this.caracter)
+    {
+      this.numero = true;
+    }
+    else
+    {
+      this.numero = false;
     }
   }
 }
